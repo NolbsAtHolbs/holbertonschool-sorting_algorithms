a199e661d2d5ccb8db270931dc25e60cbe9b36b6
NolbsAtHolbs <hikikomode@gmail.com>
2023-12-21 23:31:04 -0600

    better dll sort function

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,7 +8,7 @@
 */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current = NULL
+	listint_t *current = NULL;
 	listint_t *temp = NULL;
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
