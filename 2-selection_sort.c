0c0237a0245e1022f2c1dc93b1a4fd31bcd7b808
NolbsAtHolbs <hikikomode@gmail.com>
2023-12-22 00:59:20 -0600

    basic corrections

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -11,26 +11,26 @@ void selection_sort(int *array, size_t size)
 {
 	size_t i;
 	size_t j;
-	size_t min_index;
+	size_t mindex;
 	int temp;
 
 	for (i = 0; i < size - 1; i++)
 	{
-		min_index = i;
+		mindex = i;
 
 		for (j = i + 1; j < size; j++)
 		{
-			if (array[j] < array[min_index])
+			if (array[j] < array[mindex])
 			{
-				min_index = j;
+				mindex = j;
 			}
 		}
-		if (min_index != i)
+		if (mindex != i)
 		{
 			temp = array[i];
-			array[i] = array[min_index];
-			array[min_index] = temp;
-			printf("After swap: ");
+			array[i] = array[mindex];
+			array[mindex] = temp;
+			printf(" ");
 
 			for (j = 0; j < size; j++)
 			{
