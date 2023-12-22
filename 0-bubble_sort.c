698b9910fa19813ab02b61b63fa939c384c02d8c
NolbsAtHolbs <hikikomode@gmail.com>
2023-12-21 19:57:15 -0600

    made function

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -2,5 +2,25 @@
 
 void bubble_sort(int *array, size_t size)
 {
-    
+	size_t i, j, k;
+	int temp;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+
+				for (k = 0; k < size; k++)
+				{
+					printf("%d ", array[k]);
+				}
+				printf("\n");
+			}
+		}
+	}
 }
